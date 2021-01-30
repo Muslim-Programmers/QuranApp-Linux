f02d8ea2445f64a6071f11b0890227653698df22
nashidp <77830098+nashidp@users.noreply.github.com>
2021-01-30 08:19:26 +0530

    Minor Fix

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 FILE=./build/qapp
 if [ -f "$FILE" ]; then
-	mkdir -p /usr/share/qapp/ 
-	sudo cp ./build/qapp /usr/local/bin/
-	sudo cp ./resources/qapp-splash.jpg /usr/share/qapp/
-	sudo cp ./resources/qapp-198x198.png /usr/share/qapp/
-	sudo cp ./resources/icon.png /usr/share/qapp/
+	mkdir -p /opt/qapp/ 
+	sudo cp ./build/qapp /usr/bin/
+	sudo cp ./resources/qapp-splash.jpg /opt/qapp/
+	sudo cp ./resources/qapp-198x198.png /opt/qapp/
+	sudo cp ./resources/icon.png /opt/qapp/
 	sudo cp qapp.desktop /usr/share/applications/ 
 	sudo echo "Successfully Installed"
 	sudo echo "Open Quran app from Application Menu or type qapp --h "
